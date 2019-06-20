82390c048b95bfcbf0bff7b960d34e994f831120
zhymgc <1026467712@qq.com>
2019-06-20 11:18:43 +0800

    Initial commit

---
+++ src/main/java/com/zhymgc/infoping/InfopingApplication.java
@@ -0,0 +1,13 @@
+package com.zhymgc.infoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfopingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfopingApplication.class, args);
+    }
+
+}
